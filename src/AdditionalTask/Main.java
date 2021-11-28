c746265e04fdcce7e9925aec71c47afd6270b904
O.KH <alxxxkh@gmail.com>
2021-11-28 09:19:32 +0200

    - done

--- src/AdditionalTask/Main.java
+++ src/AdditionalTask/Main.java
@@ -1,5 +1,10 @@
 package AdditionalTask;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Vehicles myVehicles = Vehicles.BMW;
+        System.out.println(myVehicles);
+        myVehicles.setColour("red");
+        System.out.println(myVehicles);
+    }
 }
--- src/AdditionalTask/Vehicles.java
+++ src/AdditionalTask/Vehicles.java
@@ -1,4 +1,21 @@
 package AdditionalTask;
 
 public enum Vehicles {
+    HONDA(1000, "Red"), BMW(2000, "blue"), MAZDA(1500, ""), TOYOTA(1700, "");
+    private int price;
+    private String colour;
+
+    @Override
+    public String toString() {
+        return "car is " + name() + ". Price is " + price + ", colour is " + colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    Vehicles(int price, String colour) {
+        this.price = price;
+        this.colour = colour;
+    }
 }
